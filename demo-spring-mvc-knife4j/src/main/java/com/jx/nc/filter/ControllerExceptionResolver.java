affe89cd13a893276fa9ba2c0a8f4d457d6d1c4e
hedongbing <hdb@chinapostcar.com>
2021-08-17 17:28:12 +0800

    加入knife4j框架自动生成接口文档示例

---
+++ demo-spring-mvc-knife4j/src/main/java/com/jx/nc/filter/ControllerExceptionResolver.java
@@ -0,0 +1,32 @@
+package com.jx.nc.filter;
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.json.JSONUtil;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerExceptionResolver implements HandlerExceptionResolver {
+
+    @Override
+    public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
+        e.printStackTrace();
+        System.out.println(DateUtil.formatDateTime(new Date()) + "操作异常，请联系管理员");
+        try {
+            httpServletResponse.setContentType("application/json;charset=utf-8");
+            Map resultMap = new HashMap<>();
+            resultMap.put("msg", DateUtil.formatDateTime(new Date()) + "操作异常，请联系管理员");
+            httpServletResponse.getWriter().write(JSONUtil.toJsonStr(resultMap));
+            httpServletResponse.getWriter().flush();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ demo-spring-mvc-knife4j/src/main/java/com/jx/nc/webconfig/SwaggerConfiguration.java
@@ -0,0 +1,40 @@
+package com.jx.nc.webconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+@Configuration
+@EnableSwagger2WebMvc
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket defaultApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(groupApiInfo())
+                .groupName("默认接口")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.xiaominfo.knife4j.controller"))
+                //.apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo groupApiInfo(){
+        Contact contact = new Contact("knife4j演示", "www.jx.com", "");
+        return new ApiInfoBuilder()
+                .title("knife4j框架接口文档演示")
+                .description("knife4j框架接口文档演示 RESTful APIs")
+                .termsOfServiceUrl("http://www.group.com/")
+                .contact(contact)
+                .version("1.0")
+                .build();
+    }
+}
